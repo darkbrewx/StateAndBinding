b0184e277124bbad8a996d6422ef6ca5aa904613
WENBO HU <darkbrewx@gmail.com>
2023-06-28 13:47:16 +0900

    Initial Commit

---
+++ StateAndBinding/StateAndBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndBindingApp.swift
+//  StateAndBinding
+//
+//  Created by 胡文博 on 2023/06/28.
+//
+
+import SwiftUI
+
+@main
+struct StateAndBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
